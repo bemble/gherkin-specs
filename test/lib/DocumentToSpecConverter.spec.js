3013f8de985993d993dc31dc942c8ab6104f4305
Pierre CLEMENT <pierre@clement.re>
2016-05-17 15:50:37 +0200

    feat(feature): add base params matching
    related to #1

--- test/lib/DocumentToSpecConverter.spec.js
+++ test/lib/DocumentToSpecConverter.spec.js
@@ -6,12 +6,16 @@ describe('DocumentToSpecConverter', () => {
   var parser = null;
   var converter = null;
   
-  before(() => {
+  beforeEach(() => {
     parser = new Parser();
     converter = new DocumentToSpecConverter();
   });
   
   describe('convert', () => {
+    beforeEach(() => {
+      converter.matchesParams = false;
+    });
+    
     it('returns an array', (done) => {
       parser.parseFeature(`${__dirname}/../features/test1.feature`)
       .then((document) => converter.convert(document))
@@ -28,7 +32,7 @@ describe('DocumentToSpecConverter', () => {
           assert.equal(specElements[0].type, 'featureSteps');
           assert.deepEqual(specElements[0].regexp, /^Multiple site support\nAs a Mephisto site owner\nI want to host blogs for different people\nIn order to make gigantic piles of money$/);
           assert(!specElements[0].hasCallback, 'Feature does not have callback'); 
-      })
+        })
         .then(done, done);
       });
       
@@ -46,7 +50,7 @@ describe('DocumentToSpecConverter', () => {
           assert.equal(specElements[1].type, 'given');
           assert.deepEqual(specElements[1].regexp, /^a global administrator named "Greg"$/);
           assert(specElements[1].hasCallback, 'Given has callback'); 
-      })
+        })
         .then(done, done);
       });
       
@@ -57,7 +61,7 @@ describe('DocumentToSpecConverter', () => {
           assert.equal(specElements[6].type, 'when');
           assert.deepEqual(specElements[6].regexp, /^I try to post to "Expensive Therapy"$/);
           assert(specElements[6].hasCallback, 'when has callback'); 
-      })
+        })
         .then(done, done);
       });
       
@@ -68,7 +72,7 @@ describe('DocumentToSpecConverter', () => {
           assert.equal(specElements[7].type, 'then');
           assert.deepEqual(specElements[7].regexp, /^I should see "Your article was published."$/);
           assert(specElements[7].hasCallback, 'when has callback'); 
-      })
+        })
         .then(done, done);
       });
       
@@ -79,7 +83,7 @@ describe('DocumentToSpecConverter', () => {
           assert.equal(specElements[2].type, 'given');
           assert.deepEqual(specElements[2].regexp, /^a blog named "Greg's anti-tax rants"$/);
           assert(specElements[2].hasCallback, 'when has callback'); 
-      })
+        })
         .then(done, done);
       });
     });
@@ -89,6 +93,20 @@ describe('DocumentToSpecConverter', () => {
       .then((document) => converter.convert(document))
       .then((specElements) => {
         assert.equal(specElements.length, 12);
+      })
+      .then(done, done);
+    });
+  });
+  
+  describe('matchesParams', () => {
+    it('replaces ', (done) => {
+      parser.parseFeature(`${__dirname}/../features/test1.feature`)
+      .then((document) => converter.convert(document))
+      .then((specElements) => {
+        assert.equal(specElements[1].type, 'given');
+        assert.deepEqual(specElements[1].regexp, /^a global administrator named "([^"]+)"$/);
+        assert.deepEqual(specElements[1].callbackParams, ['arg1']);
+        assert(specElements[1].hasCallback, 'Given has callback'); 
       })
       .then(done, done);
     });
